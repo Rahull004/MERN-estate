806e2a7212a799eea3b864ddc0330e0649eb5dcd
Rahul Bansal <rahulbansal24072004@gmail.com>
2023-12-15 15:43:28 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,3 +32,17 @@ export const updateUser = async (req, res, next) => {
         next(error);
     }
 }
+
+export const deleteUser = async (req, res, next) => {
+
+    if (req.user.id !== req.params.id) return next(errorhandler(401, "You can only delete your own account!"))
+
+    try {
+        await User.findOneAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User has been delete!')
+        
+    } catch (error) {
+        next(error);
+    }
+};
